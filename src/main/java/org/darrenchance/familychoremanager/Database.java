77178eeecf861aa319e90c43cca7eacbdc910fe9
Darren Chance <dchance1@mail.valenciacollege.edu>
2024-04-14 22:17:32 -0400

    Created Database.java class
    This class was created as an interface to get data to and from ms sql Hibernate framework.

---
+++ src/main/java/org/darrenchance/familychoremanager/Database.java
@@ -0,0 +1,59 @@
+package org.darrenchance.familychoremanager;
+
+import javax.persistence.*;
+import java.util.List;
+
+public class Database {
+
+    /**
+     * A method to outputs all items or entities using the specified {@code Object} class name.
+     *
+     * @param typeClass the class of the entity you would like to list
+     * @return List of {@code Object} class name
+     */
+
+    public <T> List<T> listAllItems(Class<T> typeClass) {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("default");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+        try {
+            transaction.begin(); //Start
+            Query query = entityManager.createQuery("from " + typeClass.getName());
+            @SuppressWarnings("unchecked") List<T> resultList = query.getResultList();
+            transaction.commit();
+            return resultList;
+        } finally {
+            if (transaction.isActive()) {
+                transaction.rollback();
+            }
+            entityManager.close();
+            entityManagerFactory.close();
+        }
+
+    }
+
+
+    public void add(Object entity) {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("default");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+
+        try {
+            transaction.begin();
+
+            Object obj = entity;
+            System.out.println("The class name being persisted is: "+entity.getClass().getName());
+
+            entityManager.persist(obj);
+
+            transaction.commit();
+        } finally {
+            if (transaction.isActive()) {
+                transaction.rollback();
+            }
+            entityManager.close();
+            entityManagerFactory.close();
+        }
+    }
+
+}
