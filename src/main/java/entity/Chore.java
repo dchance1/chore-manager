3bf945e97308cfb8a585f2ceb993af9b4ddff143
Darren Chance <dchance1@mail.valenciacollege.edu>
2024-04-14 22:16:22 -0400

    Created Chore.java entity
    This class was created as an entity to persist objects to ms sql database through Hibernate framework.

---
+++ src/main/java/entity/Chore.java
@@ -0,0 +1,64 @@
+package entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "chore_types", schema = "dbo", catalog = "choresdb")
+public class Chore {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "id")
+    private int id;
+    @Basic
+    @Column(name = "title")
+    private String title;
+    @Basic
+    @Column(name = "description")
+    private String description;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Chore chore = (Chore) o;
+
+        if (id != chore.id) return false;
+        if (title != null ? !title.equals(chore.title) : chore.title != null) return false;
+        if (description != null ? !description.equals(chore.description) : chore.description != null) return false;
+
+        return true;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (title != null ? title.hashCode() : 0);
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        return result;
+    }
+}
