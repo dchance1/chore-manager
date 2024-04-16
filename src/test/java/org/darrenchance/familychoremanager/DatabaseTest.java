88d9c0dcf299f243260fe9e2b7a13c9bcac0f16d
Darren Chance <dchance1@mail.valenciacollege.edu>
2024-04-16 10:34:24 -0400

    Created test class

---
+++ src/test/java/org/darrenchance/familychoremanager/DatabaseTest.java
@@ -0,0 +1,12 @@
+package org.darrenchance.familychoremanager;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DatabaseTest {
+
+    @Test
+    void add() {
+    }
+}
