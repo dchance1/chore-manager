657b91dc23d5182a0b382968c9f266daa9a320b9
Darren Chance <dchance1@mail.valenciacollege.edu>
2024-04-16 18:39:08 -0400

    New test for listAllItems method #1 Testig if a non entity class passed into param that method returns null every time.

--- src/test/java/org/darrenchance/familychoremanager/DatabaseTest.java
+++ src/test/java/org/darrenchance/familychoremanager/DatabaseTest.java
@@ -1,12 +1,33 @@
 package org.darrenchance.familychoremanager;
 
+import entity.Chore;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DatabaseTest {
 
+    /**
+     * Testing if a non entity class is passed as param it will return null
+     */
     @Test
-    void add() {
+    void listAllItemsNonEntityTest() {
+
+        Database db = new Database();
+
+        // Adding non entity classes to list
+        List<Class> classes = new ArrayList();
+        classes.add(Integer.class);
+        classes.add(String.class);
+        classes.add(Database.class);
+        classes.add(Double.class);
+
+        // Testing that all non classes return null as expected
+        for (Class cl : classes) {
+            assertEquals(null, db.listAllItems(cl));
+        }
     }
 }
